6d658a944b7cf2c96a0d5dfa8d4ba18868ebadff
Silence <374619540@qq.com>
2018-03-09 20:35:30 +0800

    Init

---
+++ Demo/Others/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  Demo
+//
+//  Created by Silence on 2018/3/9.
+//  Copyright © 2018年 Silence. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+
+@end
