36fb7b3f57c52ca609b76bea05ff301d9a8f9ec2
Silence <374619540@qq.com>
2018-03-12 22:55:37 +0800

    NSNull

---
+++ SICrashGuard/NSNull/NSNull+Guard.m
@@ -0,0 +1,34 @@
+//
+//  NSNull+Guard.m
+//  Demo
+//
+//  Created by Silence on 2018/3/12.
+//  Copyright © 2018年 Silence. All rights reserved.
+//
+
+#import "SISwizzling.h"
+
+// NSNull数据Selector问题,常见于数据为空解析
+// 实现原理,将其转发给Number,String,Array,Dictionary避免出现Unrecoginzed
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wstrict-prototypes"
+
+SIStaticHookClass(NSNull, GuardNull, id, @selector(forwardingTargetForSelector:), (SEL) aSelector) {
+    static NSArray *tmpObjs ;
+    if (!tmpObjs) {
+        tmpObjs = @[@"",@0,@[],@{}];
+    }
+    
+    for (id tmpObj in tmpObjs) {
+        if ([tmpObj respondsToSelector:aSelector]) {
+            return tmpObj;
+        }
+    }
+    
+    return SIHookOrgin(aSelector);
+}
+
+SIStaticHookEnd
+
+#pragma clang diagnostic pop
