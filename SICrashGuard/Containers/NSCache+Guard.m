9e264d198c45aacd10c134449462c0af28ec5aa0
Silence <374619540@qq.com>
2018-03-14 22:07:12 +0800

    NSCache+Guard

---
+++ SICrashGuard/Containers/NSCache+Guard.m
@@ -0,0 +1,32 @@
+//
+//  NSCache+Guard.m
+//  Demo
+//
+//  Created by Silence on 2018/3/14.
+//  Copyright © 2018年 Silence. All rights reserved.
+//
+
+#import "SISwizzling.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wstrict-prototypes"
+
+SIStaticHookClass(NSCache, ProtectCont, void, @selector(setObject:forKey:), (id)obj, (id)key) {
+    if (obj && key) {
+        SIHookOrgin(obj,key);
+    } else {
+        // 错误处理
+    }
+}
+SIStaticHookEnd
+
+SIStaticHookClass(NSCache, ProtectCont, void, @selector(setObject:forKey:cost:), (id)obj, (id)key, (NSUInteger)g) {
+    if (obj && key) {
+        SIHookOrgin(obj,key,g);
+    } else {
+        // 错误处理
+    }
+}
+SIStaticHookEnd
+
+#pragma clang diagnostic pop
